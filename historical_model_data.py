4865c57bc844423508d49c24e30a2aaf99e0f482
DANA HAGAN <devwithdana@gmail.com>
2021-07-26 14:33:35 -0500

    Converted to using Ryder's excel file, verified it produced the same results.

--- historical_model_data.py
+++ historical_model_data.py
@@ -1,26 +1,26 @@
 from openpyxl import load_workbook
 
 dell_tickets_file = "C:\\Users\\hagand\\Downloads\\Dell_TechDirect_Tickets.xlsx"
-asset_tiger_file = "C:\\Users\\hagand\\Downloads\\AssetTiger_7460AIO.xlsx"
+asset_tiger_file = "C:\\Users\\hagand\\Downloads\\AssetTiger_AllDevices_Warranty.xlsx"
 
 dell_tickets_workbook = load_workbook(dell_tickets_file)
 dell_tickets_sheet = dell_tickets_workbook["Sheet1"]
 
+# Using a special exported sheet - see Ryder for details
 asset_tiger_workbook = load_workbook(asset_tiger_file)
-asset_tiger_sheet = asset_tiger_workbook["Asset"]
+asset_tiger_sheet = asset_tiger_workbook["Export"]
 
-test_dell_cell = dell_tickets_sheet['A1'].value
-print(test_dell_cell)
+searched_model = "OPTIPLEX 7460 AIO"
 
 at_device_ids = []
-for cell in asset_tiger_sheet['B']:
-  at_device_ids.append(cell.value)
-
-# Remove header from list of service tags
-at_device_ids.pop(0)
-
-print(at_device_ids[0])
-print(at_device_ids[1])
+at_device_warranties = []
+for idx, cell in enumerate(asset_tiger_sheet['A']):
+  device_st = cell.value
+  device_model = asset_tiger_sheet['D' + str(idx + 1)].value
+  device_warranty = asset_tiger_sheet['C' + str(idx + 1)].value
+  if device_model in searched_model:
+    at_device_ids.append(device_st)
+    at_device_warranties.append([device_st, device_warranty])
 
 dt_all_device_ids = []
 for cell in dell_tickets_sheet['C']:
@@ -42,3 +42,18 @@ for idx, st_id in enumerate(dt_all_device_ids):
 # WARNING: Some devices from dt_all_device_ids are the correct model we need but were not available in Asset Tiger
 # passed = "29D4XQ2" in at_device_ids
 # print(passed)
+
+#To get the number of machines - ignores multiple tickets.
+dt_devices_st_only = []
+for device in dt_devices:
+  device_st = device[0]
+  if device_st not in dt_devices_st_only:
+    dt_devices_st_only.append(device_st)
+
+
+
+
+# What we wanted to know
+print("Device count: " + str(len(at_device_ids)))
+print("Devices that have had Dell tickets: " + str(len(dt_devices_st_only)))
+print("Devices that have multiple tickets: " + str(len(dt_devices)-len(dt_devices_st_only)))
