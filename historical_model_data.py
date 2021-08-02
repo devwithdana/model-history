c1e7deefe39675ca552facc58e739542cedb9e61
DANA HAGAN <devwithdana@gmail.com>
2021-08-02 14:43:08 -0500

    Code Clean Up

--- historical_model_data.py
+++ historical_model_data.py
@@ -77,97 +77,84 @@ filtered_devices = filter_data(devices_dict)
 
 wb = Workbook()
 dest_filename = 'asset_list.xlsx'
+ws0 = wb.active
+ws0.title = "Overview"
+ws1 = wb.create_sheet()
+ws1.title = "General Asset Info"
+ws2 = wb.create_sheet()
+ws2.title = "Ticket Statistics"
+ws3 = wb.create_sheet()
+ws3.title = "Warranty to Ticket Ratio"
 
-def create_overview_assessment():
-  ws0 = wb.active
-  ws0.title = "Overview"
-
-  ws0['A1'].font = bold_font
-  ws0['A2'].font = bold_font
-  ws0['A3'].font = bold_font
-  ws0['A4'].font = bold_font
-  ws0['A5'].font = bold_font
-  ws0['A7'].font = bold_font
-  ws0['A8'].font = bold_font
-  ws0['A9'].font = bold_font
-  ws0['A10'].font = bold_font
-  ws0['A12'].font = bold_font
-  ws0['A13'].font = bold_font
-  ws0['A14'].font = bold_font
-  ws0['A15'].font = bold_font
-  ws0['A16'].font = bold_font
-  ws0['A17'].font = bold_font
-  ws0['A18'].font = bold_font
-  ws0['A19'].font = bold_font
-
+def create_overview_assessment(ws):
   # ======= Search Criteria
-  ws0['A1'] = "Search Criteria"
-  ws0['A2'] = "Service Tag"
+  ws['A1'] = "Search Criteria"
+  ws['A2'] = "Service Tag"
   if search_tag == "":
-    ws0['B2'] = "N/A"
+    ws['B2'] = "N/A"
   else:
-    ws0['B2'] = search_tag
+    ws['B2'] = search_tag
 
-  ws0['A3'] = "Model"
+  ws['A3'] = "Model"
   if search_model == "":
-    ws0['B3'] = "N/A"
+    ws['B3'] = "N/A"
   else:
-    ws0['B3'] = search_model
+    ws['B3'] = search_model
 
-  ws0['A4'] = "Purchase Date"
+  ws['A4'] = "Purchase Date"
   if search_purchase_date == "":
-    ws0['B4'] = "N/A"
+    ws['B4'] = "N/A"
   else:
-    ws0['B4'] = search_purchase_date
+    ws['B4'] = search_purchase_date
 
-  ws0['A5'] = "Warranty Date"
+  ws['A5'] = "Warranty Date"
   if search_warranty == "":
-    ws0['B5'] = "N/A"
+    ws['B5'] = "N/A"
   else:
-    ws0['B5'] = search_warranty
+    ws['B5'] = search_warranty
 
   # ======= Result Info
-  ws0['A7'] = "Device Count"
-  ws0['B7'] = len(filtered_devices)
+  ws['A7'] = "Device Count"
+  ws['B7'] = len(filtered_devices)
 
   expired = {k:v for (k, v) in filtered_devices.items() if v.hasExpired()}
-  ws0['A8'] = "Out of Warranty"
-  ws0['B8'] = len(expired)
+  ws['A8'] = "Out of Warranty"
+  ws['B8'] = len(expired)
 
   has_tickets = {k:v for (k, v) in filtered_devices.items() if len(v.tickets) > 0}
-  ws0['A9'] = "Have Had Tickets"
-  ws0['B9'] = len(has_tickets)
+  ws['A9'] = "Have Had Tickets"
+  ws['B9'] = len(has_tickets)
 
   have_multiple_tickets = {k:v for (k, v) in has_tickets.items() if len(v.tickets) > 1}
-  ws0['A10'] = "Have Had Multiple Tickets"
-  ws0['B10'] = len(have_multiple_tickets)
+  ws['A10'] = "Have Had Multiple Tickets"
+  ws['B10'] = len(have_multiple_tickets)
 
-  ws0['A12'] = "Ticket Categories"
+  ws['A12'] = "Ticket Categories"
 
   mobo_issues = {k:v for (k, v) in has_tickets.items() if v.tickets[0].category == "MOBO"}
-  ws0['A13'] = "Motherboard"
-  ws0['B13'] = len(mobo_issues)
+  ws['A13'] = "Motherboard"
+  ws['B13'] = len(mobo_issues)
 
   hdd_issues = {k:v for (k, v) in has_tickets.items() if v.tickets[0].category == "HDD"}
-  ws0['A14'] = "Hard Drive"
-  ws0['B14'] = len(hdd_issues)
+  ws['A14'] = "Hard Drive"
+  ws['B14'] = len(hdd_issues)
   
   lcd_issues = {k:v for (k, v) in has_tickets.items() if v.tickets[0].category == "LCD"}
-  ws0['A15'] = "LCD/Screen"
-  ws0['B15'] = len(lcd_issues)
+  ws['A15'] = "LCD/Screen"
+  ws['B15'] = len(lcd_issues)
 
   psu_issues = {k:v for (k, v) in has_tickets.items() if v.tickets[0].category == "PSU"}
-  ws0['A16'] = "Power Supply"
-  ws0['B16'] = len(psu_issues)
+  ws['A16'] = "Power Supply"
+  ws['B16'] = len(psu_issues)
 
   ram_issues = {k:v for (k, v) in has_tickets.items() if v.tickets[0].category == "RAM"}
-  ws0['A17'] = "RAM/Memory"
-  ws0['B17'] = len(ram_issues)
+  ws['A17'] = "RAM/Memory"
+  ws['B17'] = len(ram_issues)
 
   other_issues = {k:v for (k, v) in has_tickets.items() if v.tickets[0].category == "OTHER"}
-  ws0['A18'] = "Other"
-  ws0['B18'] = len(other_issues)
-  ws0['A19'] = "Other Issues Include:"
+  ws['A18'] = "Other"
+  ws['B18'] = len(other_issues)
+  ws['A19'] = "Other Issues Include:"
   idx = 19
   for device in other_issues:
     tickets = other_issues[device].tickets
@@ -175,25 +162,17 @@ def create_overview_assessment():
     for ticket in tickets:
       if ticket.problem not in unique_tickets:
         unique_tickets.append(ticket.problem)
-        ws0['B' + str(idx)] = ticket.problem
+        ws['B' + str(idx)] = ticket.problem
         idx = idx + 1
 
-  set_col_width(ws0)
-
-def create_gen_asset_info_ws():
-  ws1 = wb.create_sheet()
-  ws1.title = "General Asset Info"
-
+def create_gen_asset_info_ws(ws):
   # Set Headers
-  ws1['A1'] = "Purchased"
-  ws1['B1'] = "Model"
-  ws1['C1'] = "Service Tag"
-  ws1['D1'] = "Warranty Expiration"
-  ws1['E1'] = "Tickets"
-  ws1['F1'] = "Ticket Categories"
-
-  for cell in ws1[1]:
-    cell.font = bold_font
+  ws['A1'] = "Purchased"
+  ws['B1'] = "Model"
+  ws['C1'] = "Service Tag"
+  ws['D1'] = "Warranty Expiration"
+  ws['E1'] = "Tickets"
+  ws['F1'] = "Ticket Categories"
 
   idx = 2
   for asset in filtered_devices:
@@ -201,61 +180,48 @@ def create_gen_asset_info_ws():
     p_date = ""
     we_date_config = str(device.warranty_expiration).split()[0].split("-")
     we_date = we_date_config[1] + "-" + we_date_config[2] + "-" + we_date_config[0]
-    ws1['A' + str(idx)] = format_date(device.purchase_date)
-    ws1['B' + str(idx)] = device.model
-    ws1['C' + str(idx)] = device.service_tag
-    ws1['D' + str(idx)] = format_date(device.warranty_expiration)
-    ws1['E' + str(idx)] = len(device.tickets)
+    ws['A' + str(idx)] = format_date(device.purchase_date)
+    ws['B' + str(idx)] = device.model
+    ws['C' + str(idx)] = device.service_tag
+    ws['D' + str(idx)] = format_date(device.warranty_expiration)
+    ws['E' + str(idx)] = len(device.tickets)
     problem_types = ""
     for ticket in device.tickets:
       cat = ticket.category
       if cat not in problem_types:
         problem_types = problem_types + " " + cat
-    ws1['F' + str(idx)] = problem_types
+    ws['F' + str(idx)] = problem_types
     idx = idx + 1
 
-  set_col_width(ws1)
+  set_col_width(ws)
 
-def create_ticket_view_ws():
-  ws2 = wb.create_sheet()
-  ws2.title = "Ticket Statistics"
-
+def create_ticket_view_ws(ws):
   has_tickets = {k:v for (k, v) in filtered_devices.items() if len(v.tickets) > 0}
   # Set Headers
-  ws2['A1'] = "Service Tag"
-  ws2['B1'] = "Warranty Expiration Date"
-  ws2['C1'] = "Ticket Created"
-  ws2['D1'] = "Category"
-  ws2['E1'] = "Problem"
-  ws2['F1'] = "Status"
-
-  for cell in ws2[1]:
-    cell.font = bold_font
+  ws['A1'] = "Service Tag"
+  ws['B1'] = "Warranty Expiration Date"
+  ws['C1'] = "Ticket Created"
+  ws['D1'] = "Category"
+  ws['E1'] = "Problem"
+  ws['F1'] = "Status"
 
   idx = 2
   for asset in has_tickets:
     device = has_tickets[asset]
     tickets = device.tickets
     for ticket in tickets:
-      ws2['A' + str(idx)] = device.service_tag
-      ws2['B' + str(idx)] = format_date(device.warranty_expiration)
-      ws2['C' + str(idx)] = format_date(ticket.date_created)
-      ws2['D' + str(idx)] = ticket.category
-      ws2['E' + str(idx)] = ticket.problem
-      ws2['F' + str(idx)] = ticket.status
+      ws['A' + str(idx)] = device.service_tag
+      ws['B' + str(idx)] = format_date(device.warranty_expiration)
+      ws['C' + str(idx)] = format_date(ticket.date_created)
+      ws['D' + str(idx)] = ticket.category
+      ws['E' + str(idx)] = ticket.problem
+      ws['F' + str(idx)] = ticket.status
       idx = idx + 1
-  set_col_width(ws2)
 
-def create_warranty_ticket_ratios():
-  ws3 = wb.create_sheet()
-  ws3.title = "Warranty to Ticket Ratio"
-
+def create_warranty_ticket_ratios(ws):
   # Key = Month-Year, Value = [Expiring, Tickets]
-  ws3['B1'] = "Devices Expiring"
-  ws3['C1'] = "Tickets Created"
-
-  for cell in ws3[1]:
-    cell.font = bold_font
+  ws['B1'] = "Devices Expiring"
+  ws['C1'] = "Tickets Created"
 
   ratio = dict()
   for asset in filtered_devices:
@@ -284,11 +250,10 @@ def create_warranty_ticket_ratios():
   for key in sorted (ratio.keys()):
     date = ratio[key]
     display_date = key.split("-")
-    ws3['A' + str(idx)] = display_date[1] + "-" + display_date[0]
-    ws3['B' + str(idx)] = date[0]
-    ws3['C' + str(idx)] = date[1]
+    ws['A' + str(idx)] = display_date[1] + "-" + display_date[0]
+    ws['B' + str(idx)] = date[0]
+    ws['C' + str(idx)] = date[1]
     idx = idx + 1
-  set_col_width(ws3)
 
 def set_col_width(ws):
   col_widths = []
@@ -306,9 +271,25 @@ def set_col_width(ws):
     # +1 for wiggle room
     ws.column_dimensions[get_column_letter(i + 1)].width = col_width + 1
 
+def style_workbook(ws_arr):
+  for row in ws_arr[0]:
+    row[0].font = bold_font
 
-create_overview_assessment()
-create_gen_asset_info_ws()
-create_ticket_view_ws()
-create_warranty_ticket_ratios()
+  for cell in ws_arr[1][1]:
+    cell.font = bold_font
+  for cell in ws_arr[2][1]:
+    cell.font = bold_font
+  for cell in ws_arr[3][1]:
+    cell.font = bold_font
+
+  set_col_width(ws_arr[0])
+  set_col_width(ws_arr[1])
+  set_col_width(ws_arr[2])
+  set_col_width(ws_arr[3])
+
+create_overview_assessment(ws0)
+create_gen_asset_info_ws(ws1)
+create_ticket_view_ws(ws2)
+create_warranty_ticket_ratios(ws3)
+style_workbook([ws0, ws1, ws2, ws3])
 wb.save(filename = dest_filename)
